cd1866343378a470ee7f11422693b7e863fa44ca
nicolasgameover <nicolasgameover@outlook.com>
2024-08-11 08:48:20 -0300

    Create operacoes.js

---
+++ Cursos/curso-em-video/javascript/aula-07/operacoes.js
@@ -0,0 +1,19 @@
+ // Aritimeticos
+console.log('Soma de cinco mais dois:')
+console.log(`5 + 2 = `,5 + 2);  // soma
+console.log()
+console.log('Subtracao de cinco menos dois:')
+console.log(`5 - 2 = `,5 - 2);  // subtracao
+console.log()
+console.log('Multiplicacao de cinco vezes dois')
+console.log(`5 * 5 = `,5 * 2);  // multiplicacao
+console.log()
+console.log('Pontecia de cinco elevado ao quadrado:')
+console.log(`5 ** 5 = `,5 ** 2); // potenciacao
+console.log()
+console.log('Divisao de cinco dividido por dois')
+console.log(`5 / 2 = `,5 / 2);  // divisao
+console.log()
+console.log('Modulo de cinco por dois:')
+console.log(`5 % 2 = `,5 % 2);  // modulo
+console.log()
